daa7b9de49d86a572c88f224e38917e3e727af0f
Navin D Bhudiya(Certified Magento 2x Developer) <navin@magento360.com>
2025-06-10 09:40:19 +0530

    Block Bugbot added

---
+++ Block/BugBot.php
@@ -0,0 +1,15 @@
+<?php
+
+class BugBot
+{
+    protected $_productRepository;
+    public function __construct(
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+    ) {
+        $this->_productRepository = $productRepository;
+    }
+    public function getProduct($productId){
+        $product = $this->_productRepository->getById($productId);
+        return $product;
+    }
+}
